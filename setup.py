e94ddb4ed837afd355689e1e69fad76ce60132ba
Mateusz Wykurz <wykurz@gmail.com>
2018-05-03 00:46:34 -0400

    Update setup.py - make install_requires less restrictive, see [link](https://packaging.python.org/discussions/install-requires-vs-requirements/) for rationale - fix indentation

--- setup.py
+++ setup.py
@@ -1,28 +1,28 @@
 from setuptools import setup, find_packages
 
 setup(
-      name='gdax-python-api',
-      version='0.2',
-      description='GDAX API client written in Python3 using async/await',
-      url='https://github.com/csko/gdax-python-api',
-      license='MIT License',
-      keywords='gdax gdax-api gdax-python python3 python-3.6 coinbase '
-               'coinbase-api bitcoin ethereum litecoin trading-api',
-      author='Kornel Csernai',
-      author_email='cskornel@gmail.com',
-      install_requires=[
-        'aiohttp==3.0.9',
-        'aiofiles==0.3.2',
-        'async_timeout==2.0.0',
-        'sortedcontainers==1.5.9',
-      ],
-      packages=find_packages(),
-      include_package_data=True,
-      platforms='any',
-      tests_require=[
+    name='gdax-python-api',
+    version='0.2',
+    description='GDAX API client written in Python3 using async/await',
+    url='https://github.com/csko/gdax-python-api',
+    license='MIT License',
+    keywords=('gdax gdax-api gdax-python python3 python-3.6 coinbase '
+              'coinbase-api bitcoin ethereum litecoin trading-api'),
+    author='Kornel Csernai',
+    author_email='cskornel@gmail.com',
+    install_requires=[
+        'aiohttp>=2.2,<=3',
+        'aiofiles==0.3',
+        'async_timeout>=1.2,<=2',
+        'sortedcontainers>=1.5,<2',
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    platforms='any',
+    tests_require=[
         'pytest',
-      ],
-      classifiers=[
+    ],
+    classifiers=[
         'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.6',
         'Development Status :: 4 - Beta',
@@ -32,5 +32,5 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Topic :: Software Development :: Libraries :: Python Modules',
-      ],
+    ],
 )
